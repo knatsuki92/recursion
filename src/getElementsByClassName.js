1d63112e818ae92f3e68a2b8b71ce054f905fa07
Kuni Natsuki <knatsuki92@gmail.com>
2015-01-08 00:52:26 +0900

    getElementsByClassName() done using recursion. 	Note: the code could still use some cleaning.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,38 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+
+//Outline:
+//document.body - the grand-daddy node.
+//element.childNodes - returns an array of all the child nodes directly under element node.
+//element.classList - returns an an array containing all the class names for the element node. 
+
+var getElementsByClassName = function(className){
+  // This should return an array of all the elements with the class 'className'. 
+
+  var returnArray = []; //the output array
+
+  var checkInClass = function(element, className){
+  	var classList = element.classList;
+
+  	//First, push the element into returnArray if it's under the class 'className'.
+  	if(classList !== undefined){ //only check for appropriate elements.
+		for(var i = 0; i < classList.length; i++){
+			if(classList[i] === className){
+				returnArray.push(element);
+			}
+		}
+	}
+	//Next, recurse through all its child elements, child-child elements...and so on.
+	var childElements = element.childNodes;
+	for(var j = 0; j < childElements.length; j++){
+		checkInClass(childElements[j], className);
+	}
+  }
+
+  //Start from Eve.
+  checkInClass(document.body, className);
+  return returnArray;
+
+
 };
