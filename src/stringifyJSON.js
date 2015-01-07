710a877c1c41a74c7c17ae5760ac22f1a51e9840
Kuni Natsuki <knatsuki92@gmail.com>
2015-01-07 21:27:07 +0900

    This ver. passes the stringifyJSONspec.js test. 	- implemented the cases for null, functions, and undefined.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -17,15 +17,22 @@ var stringifyJSON = function(obj) {
 		stringifyJSON(1) = "1"
 		stringifyJSON(true) = "true"
 		stringifyJSON('true') = ""true""
+	case 4: null
+		stringifyJSON(null) = "null"
   */
 	var outputJSON = "";
-
+	//case: null
+	if(obj === null){
+		outputJSON += obj;
+		return outputJSON;
+	}
 	//case: value
-	if(!Array.isArray(obj) && typeof(obj) !== 'object'){
+	else if(!Array.isArray(obj) && typeof(obj) !== 'object'){
 		outputJSON += obj;
 		if(typeof(obj) === 'string'){
 			outputJSON = "\"" + outputJSON + "\""; //strings are double-quoted.
 		}
+		return outputJSON;
 	}
 	//case: array
 	//*NOTE: it's crucial for array case to be considered before the object case since typeof(array) === 'object' is true.
@@ -34,19 +41,30 @@ var stringifyJSON = function(obj) {
 		for(var i = 0; i < obj.length; i++){
 			outputJSON += stringifyJSON(obj[i]) + ","; //using recursion here as well.
 		}
-		outputJSON = outputJSON.substr(0,outputJSON.length-1); //delete the last comma.
+		if(obj.length > 0) { //only if it isn't an empty array
+			outputJSON = outputJSON.substr(0,outputJSON.length - 1); //delete the last comma.
+		}
 		outputJSON += "]";
+		return outputJSON;
 	}
 	//case: object
 	else if(typeof(obj) === 'object'){
 		outputJSON += "{"
+
+		var numChild = 0; //check for empty set.
 		for(var child in obj){
-			outputJSON += "\"" + child + "\":" + stringifyJSON(obj[child]) + ","; //using recursion here.
+			if(typeof(obj[child]) !== 'function' && obj[child] !== undefined){ //skip for functions and undefined.
+				outputJSON += "\"" + child + "\":" + stringifyJSON(obj[child]) + ","; //using recursion here.
+				numChild += 1;
+			}
 		}
-		outputJSON = outputJSON.substr(0,outputJSON.length-1); //delete the last comma.
+		if(numChild !== 0){
+			outputJSON = outputJSON.substr(0,outputJSON.length - 1); //delete the last comma.
+		}
 		outputJSON += "}";
+		return outputJSON;
 	}
 
-	return outputJSON;
+	
 
 };
